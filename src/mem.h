3ff256da37e3dbadebe225f11d93688dc21427c5
barbarossa12 <saratheedewmith325@gmail.com>
2026-08-21 00:32:57 +1000

    changed parameter name for the write functions from data to val.

--- src/mem.h
+++ src/mem.h
@@ -17,7 +17,7 @@ uint16_t mem_read16(Memory *mem, uint32_t addr);
 uint32_t mem_read32(Memory *mem, uint32_t addr);
 
 /*memory write functions*/
-void mem_write8(Memory *mem, uint32_t addr, uint8_t data);
-void mem_write16(Memory *mem, uint32_t addr, uint16_t data);
-void mem_write32(Memory *mem, uint32_t addr, uint16_t data);
+void mem_write8(Memory *mem, uint32_t addr, uint8_t val);
+void mem_write16(Memory *mem, uint32_t addr, uint16_t val);
+void mem_write32(Memory *mem, uint32_t addr, uint16_t val);
 
