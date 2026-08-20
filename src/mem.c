c1aea56c30395348346cd1e074acf33078081dd5
barbarossa12 <saratheedewmith325@gmail.com>
2026-08-21 00:21:46 +1000

    Implemented the memory read functions.

---
+++ src/mem.c
@@ -0,0 +1,47 @@
+#include "log.h"
+#include "mem.h"
+
+void mem_init(Memory *mem, size_t size) {
+	mem->buffer = (uint8_t*)malloc(size);
+	if(mem->buffer) {
+		mem->size = size;
+	} else {
+		LOG_FATAL("Failed to allocate memory: malloc falied");
+}
+
+void mem_free(Memory *mem) {
+	free(mem->buffer);
+	mem->buffer = NULL;
+	mem->size = 0;
+	LOG_INFO("Memory freed!");
+}
+
+/*memory read functions*/
+uint8_t mem_read8(Memory *mem, uint32_t addr) {
+	if(addr >= mem->size) {
+		LOG_FATAL("out-of-bound read at address 0x%08x (buffer size %zu)", address, mem->size);
+	}
+	return mem->buffer[addr];
+}
+
+uint16_t mem_read16(Memory *mem, uint32_t addr) {
+	if(addr + 1 >= mem->size) {
+		LOG_FATAL("out-of-bound read at address 0x%08x (buffer size %zu)", address, mem->size);
+	}
+	uint16_t result = (mem->buffer[addr+1] << 8);
+	result |= mem->buffer[addr];
+	return result;
+}
+
+uint32_t mem_read32(Memory *mem, uint32_t addr) {
+	if(addr + 3 >= mem->size) {
+		LOG_FATAL("out-of-bound read at address 0x%08x (buffer size %zu)", address, mem->size);
+	}
+	uint32_t result = (mem->buffer[addr + 3]) << 24;
+	result |= (mem->buffer[addr + 2]) << 16
+}
+
+/*memory write functions*/
+void mem_write8(Memory *mem, uint32_t addr, uint8_t data);
+void mem_write16(Memory *mem, uint32_t addr, uint16_t data);
+void mem_write32(Memory *mem, uint32_t addr, uint16_t data);
