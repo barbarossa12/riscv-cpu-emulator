4c69aa6b459143c06c5edde70ff331557870b2b0
barbarossa12 <saratheedewmith325@gmail.com>
2026-08-21 00:34:32 +1000

    Fixed minor typos and aded the write functions.

--- src/mem.c
+++ src/mem.c
@@ -7,6 +7,7 @@ void mem_init(Memory *mem, size_t size) {
 		mem->size = size;
 	} else {
 		LOG_FATAL("Failed to allocate memory: malloc falied");
+	}
 }
 
 void mem_free(Memory *mem) {
@@ -19,14 +20,14 @@ void mem_free(Memory *mem) {
 /*memory read functions*/
 uint8_t mem_read8(Memory *mem, uint32_t addr) {
 	if(addr >= mem->size) {
-		LOG_FATAL("out-of-bound read at address 0x%08x (buffer size %zu)", address, mem->size);
+		LOG_FATAL("out-of-bound read at address 0x%08x (buffer size %zu)", addr, mem->size);
 	}
 	return mem->buffer[addr];
 }
 
 uint16_t mem_read16(Memory *mem, uint32_t addr) {
 	if(addr + 1 >= mem->size) {
-		LOG_FATAL("out-of-bound read at address 0x%08x (buffer size %zu)", address, mem->size);
+		LOG_FATAL("out-of-bound read at address 0x%08x (buffer size %zu)", addr, mem->size);
 	}
 	uint16_t result = (mem->buffer[addr+1] << 8);
 	result |= mem->buffer[addr];
@@ -35,13 +36,38 @@ uint16_t mem_read16(Memory *mem, uint32_t addr) {
 
 uint32_t mem_read32(Memory *mem, uint32_t addr) {
 	if(addr + 3 >= mem->size) {
-		LOG_FATAL("out-of-bound read at address 0x%08x (buffer size %zu)", address, mem->size);
+		LOG_FATAL("out-of-bound read at address 0x%08x (buffer size %zu)", addr, mem->size);
 	}
 	uint32_t result = (mem->buffer[addr + 3]) << 24;
-	result |= (mem->buffer[addr + 2]) << 16
+	result |= (mem->buffer[addr + 2]) << 16;
+	result |= (mem->buffer[addr + 1]) << 8;
+	result |= (mem->buffer[addr]);
+	return result;
 }
 
 /*memory write functions*/
-void mem_write8(Memory *mem, uint32_t addr, uint8_t data);
-void mem_write16(Memory *mem, uint32_t addr, uint16_t data);
-void mem_write32(Memory *mem, uint32_t addr, uint16_t data);
+void mem_write8(Memory *mem, uint32_t addr, uint8_t val) {
+	if(addr >= mem->size) {
+		LOG_FATAL("out-of-bound write at address 0x%08x (buffer size %zu)", addr, mem->size);
+	}
+	mem->buffer[addr] = val;
+}
+
+void mem_write16(Memory *mem, uint32_t addr, uint16_t val) {
+	if(addr + 1 >= mem->size) {
+		LOG_FATAL("out-of-bound write at address 0x%08x (buffer size %zu)", addr, mem->size);
+	}
+	mem->buffer[addr] = val & 0xFF;
+	mem->buffer[addr + 1] = (val >> 8) & 0xFF;
+}
+
+void mem_write32(Memory *mem, uint32_t addr, uint16_t val) {
+	if(addr + 3 >=  mem->size) {
+		LOG_FATAL("out-of-bound write at address 0x%08x (buffer size %zu)", addr, mem->size);
+	}
+	mem->buffer[addr] = val & 0xFF;
+	mem->buffer[addr + 1] = (val >> 8) & 0xFF;
+	mem->buffer[addr + 2] = (val >> 16) & 0xFF;
+	mem->buffer[addr + 3] = (val >> 24) & 0xFF;
+}
+
