eb177ceebca2aad483ec4ab3ba7fb5d1ece65699
barbarossa12 <saratheedewmith325@gmail.com>
2026-08-21 18:12:02 +1000

    Added missing mem fre for write 32 byte order and implemented test write 16 byte order. removed unnecessary uint32_t castings.

--- tests/test_mem.c
+++ tests/test_mem.c
@@ -17,19 +17,38 @@ void test_write_then_read_8bit_roundtrip(void) {
 void test_mem_write32_byte_order(void) {
     Memory test_mem;
     mem_init(&test_mem, 128);
-    mem_write32(&test_mem, 0, (uint32_t)0xAABBCCDD);
+    mem_write32(&test_mem, 0, 0xAABBCCDD);
     uint32_t result = mem_read32(&test_mem, 0);
 
-    TEST_ASSERT_EQUAL_UINT32((uint32_t)0xAABBCCDD, result);
+    TEST_ASSERT_EQUAL_UINT32(0xAABBCCDD, result);
     TEST_ASSERT_EQUAL_UINT8(0xDD, test_mem.buffer[0]);
     TEST_ASSERT_EQUAL_UINT8(0xCC, test_mem.buffer[1]);
     TEST_ASSERT_EQUAL_UINT8(0xBB, test_mem.buffer[2]);
     TEST_ASSERT_EQUAL_UINT8(0xAA, test_mem.buffer[3]);
+    mem_free(&test_mem);
 }
+
+
+void test_mem_write16_byte_order(void) {
+    Memory test_mem;
+    mem_init(&test_mem, 128);
+    mem_write16(&test_mem, 0, 0xAABB);
+    uint16_t result = mem_read16(&test_mem, 0);
+
+    // testing the readback
+    TEST_ASSERT_EQUAL_UINT16(0xAABB, result);
+    
+    // testing the byte ordering
+    TEST_ASSERT_EQUAL_UINT8(0xBB, test_mem.buffer[0]);
+    TEST_ASSERT_EQUAL_UINT8(0xAA, test_mem.buffer[1]);
+    mem_free(&test_mem);
+}
+
 
 int main(void) {
 	UNITY_BEGIN();
 	RUN_TEST(test_write_then_read_8bit_roundtrip);
 	RUN_TEST(test_mem_write32_byte_order);
+    RUN_TEST(test_mem_write16_byte_order);
 	return UNITY_END();
 }
