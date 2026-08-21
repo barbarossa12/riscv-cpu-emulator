de3f80bf6349325079bbd49de414bb7481cc8fb7
barbarossa12 <saratheedewmith325@gmail.com>
2026-08-21 12:51:42 +1000

    Added CPU read write implementations.

---
+++ src/cpu.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include "log.h"
+#include "cpu.h"
+uint32_t cpu_read_reg(CPU *cpu, uint8_t index) {
+	if(index > 31) {
+		LOG_FATAL("CPU read register value larger than 32: %d", index);
+	}
+
+	if(index == 0) {
+		return (uint32_t) 0;
+	}
+
+	return cpu->regs[index];
+
+}
+
+void cpu_write_reg(CPU *cpu, uint8_t index, uint32_t value) {
+	if(index > 31) {
+		LOG_FATAL("CPU write register value larger than 32: %d", index);
+	}
+
+	if(index == 0) {
+		cpu->regs[index] = 0;
+	}
+
+	cpu->regs[index] = value;
+}
+
