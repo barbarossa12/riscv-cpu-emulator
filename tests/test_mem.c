f0387251007c87595064b3f73e396397e57422e9
barbarossa12 <saratheedewmith325@gmail.com>
2026-08-21 18:39:20 +1000

    Added test case to test memory last valid address.

--- tests/test_mem.c
+++ tests/test_mem.c
@@ -37,11 +37,46 @@ void test_mem_write16_byte_order(void) {
 
     // testing the readback
     TEST_ASSERT_EQUAL_UINT16(0xAABB, result);
-    
+
     // testing the byte ordering
     TEST_ASSERT_EQUAL_UINT8(0xBB, test_mem.buffer[0]);
     TEST_ASSERT_EQUAL_UINT8(0xAA, test_mem.buffer[1]);
     mem_free(&test_mem);
+}
+
+void test_mem_boundary_last_valid_address(void) {
+
+    // test for 8 bit read and write
+    Memory test_mem8;
+    mem_init(&test_mem8, 16);
+
+    mem_write8(&test_mem8, 15, 0xAA);
+    uint8_t result_8bit = mem_read8(&test_mem8, 15);
+
+    //test the readback value
+    TEST_ASSERT_EQUAL_UINT8(0xAA, result_8bit);
+
+    // test for 16 bit read and write
+    Memory test_mem16;
+    mem_init(&test_mem16, 16);
+
+    mem_write16(&test_mem16, 14, 0xAABB);
+    uint16_t result_16bit = mem_read16(&test_mem16, 14);
+    TEST_ASSERT_EQUAL_UINT16(0xAABB, result_16bit);
+
+    // test for the 32bit read and write
+    Memory test_mem32;
+    mem_init(&test_mem32, 16);
+
+    mem_write32(&test_mem32, 12, 0xAABBCCDD);
+    uint32_t result_32bit = mem_read32(&test_mem32, 12);
+    TEST_ASSERT_EQUAL_UINT32(0xAABBCCDD, result_32bit);
+
+
+    mem_free(&test_mem8);
+    mem_free(&test_mem16);
+    mem_free(&test_mem32);
+
 }
 
 
@@ -50,5 +85,7 @@ int main(void) {
 	RUN_TEST(test_write_then_read_8bit_roundtrip);
 	RUN_TEST(test_mem_write32_byte_order);
     RUN_TEST(test_mem_write16_byte_order);
+    RUN_TEST(test_mem_boundary_last_valid_address);
+
 	return UNITY_END();
 }
